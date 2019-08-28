5f1b2d34e882e057c41858b82811015a6677e796
ToothColdWoo <upup1810@naver.com>
2019-08-29 01:51:24 +0900

    Fix all ERR and make Footer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from 'components/App';
+import { Provider } from "react-redux";
+import store, { history } from "./redux/configureStore";
+import { ConnectedRouter } from "connected-react-router";
+import I18n from 'redux-i18n'
+import { translations } from "translations"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+	<Provider store={store}>
+		<ConnectedRouter history={history}>
+            <I18n translations={translations} initialLang="en" fallbackLang="en">
+				<App />
+            </I18n>
+		</ConnectedRouter>
+	</Provider>,
+rootElement);
---
+++ frontend/src/redux/configureStore.js
@@ -0,0 +1,44 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { createBrowserHistory } from "history";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { i18nState } from "redux-i18n";
+import user from 'redux/modules/user';
+
+
+
+const env = process.env.NODE_ENV;
+const history = createBrowserHistory();
+const middlewares = [thunk, routerMiddleware(history)];
+
+if (env === "development") {
+	const { logger } = require("redux-logger");
+	middlewares.push(logger);
+}
+
+const reducer = combineReducers({   // combine every reducers
+    user,
+    router: connectRouter(history),
+    i18nState
+});
+
+
+let store;
+
+if (env === "development") {
+  store = initialState =>
+    createStore(
+      reducer, 
+      composeWithDevTools(applyMiddleware(...middlewares))
+    );
+} else {
+  store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+}
+
+export { history }; // router need history object that I created 
+
+export default store(); // made store with many reducers
+
+// redux middlewares is located in between react application and store
+// redux-thunk : let us send actions to our redux sotre when we want 
